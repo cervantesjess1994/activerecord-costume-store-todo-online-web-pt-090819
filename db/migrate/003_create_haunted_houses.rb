1fd9a626af39830eef90b69cdb8eb9b7c9acbf27
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-11-27 13:33:09 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,11 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_houses do |t|
+      :name
+      :location
+      :theme
+      :price
+      :is_family_friendly
+      t.timestamp :opening_date
+      t.timestamp :closing_date
+      t.text :long_description
