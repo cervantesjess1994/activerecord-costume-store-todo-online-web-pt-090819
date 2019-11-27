2a5504220ad3af086674ce22da19b3a508f87d43
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-11-27 13:29:11 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.text :employees_count
+      t.string :employees_count
       t.boolean :still_in_business
       t.timestamp :opening_time
       t.timestamp :closing_time
