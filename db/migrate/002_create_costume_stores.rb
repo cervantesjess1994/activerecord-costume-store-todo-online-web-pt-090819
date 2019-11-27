bdb18e3cc2b7ff0f36edf312e0a0e1ff8d08ec92
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-11-27 13:18:46 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,11 +1,11 @@
 class CreateCostumesStores < ActiveRecord::Migration[5.2]
   def change
     create_table :costume_stores do |t|
-      :name
-      :location
-      :costume_inventory
-      :mun_of_employees
-      :in_business
+      t.string :name
+      t.text :location
+      t.integer :costume_inventory
+      t.integer :mun_of_employees
+      t.boolean :in_business
       t.timestamps
     end
   end
